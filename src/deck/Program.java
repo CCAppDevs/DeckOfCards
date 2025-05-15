12aa151bcbbf71d0a444d843fb19322125df9e20
Jesse Harlan <jesse.harlan@centralia.edu>
2025-05-15 12:55:37 -0700

    added a comment about peek testing

--- src/deck/Program.java
+++ src/deck/Program.java
@@ -5,13 +5,13 @@ public class Program {
 	public static void main(String[] args) {
 		Deck myDeck = new Deck();
 		
+		// peek test
 		System.out.println(myDeck.peekCard(999));
 		System.out.println(myDeck.peekCard(9));
 		
 		for (int i = 0; i < 52; i++) {
 			System.out.println(myDeck.drawCard());
 		}
-		
 
 	}
 
